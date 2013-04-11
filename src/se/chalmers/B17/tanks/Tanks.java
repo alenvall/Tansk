44b042fe8ffd18e44d0aa10c3ee8d13f48c2eb5f
Kirtez <kirtez@gmail.com>
2013-04-11 10:44:02 +0200

    Test commit with libs.

---
+++ src/se/chalmers/B17/tanks/Tanks.java
@@ -0,0 +1,13 @@
+package se.chalmers.B17.tanks;
+
+public class Tanks {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hej");
+	}
+
+}
