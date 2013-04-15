cf5eff0c98ed4562219ed32afafd429ce06e10a9
killcone <vicsand@student.chalmers.se>
2013-04-15 16:40:56 +0200

    AbstractWeapon now holds a projectiletype and a firerate.

--- src/chalmers/TDA367/B17/model/AbstractWeapon.java
+++ src/chalmers/TDA367/B17/model/AbstractWeapon.java
@@ -2,8 +2,29 @@ package chalmers.TDA367.B17.model;
 
 public abstract class AbstractWeapon {
 
-	public AbstractWeapon() {
-		// TODO Auto-generated constructor stub
+	private final AbstractProjectile projectileType;
+	private int fireRate; // milliseconds
+	
+	public AbstractWeapon(AbstractProjectile projectileType, int fireRate) {
+		this.projectileType = projectileType;
+		this.fireRate = fireRate;
+	}
+
+	public AbstractProjectile getProjectileType() {
+		return projectileType;
+	}
+
+	protected void setProjectileType(AbstractProjectile projectileType) {
+		//this.projectileType = projectileType;
+		//Shouldn't do anything.
+	}
+
+	public double getFireRate() {
+		return fireRate;
+	}
+
+	public void setFireRate(int fireRate) {
+		this.fireRate = fireRate;
 	}
 
 }
