0a7cd37b15809c472e38a04897276f345c8a6392
killcone <vicsand@student.chalmers.se>
2013-04-15 15:57:57 +0200

    Added a name for AbstractPowerUp.

--- src/chalmers/TDA367/B17/model/AbstractPowerUp.java
+++ src/chalmers/TDA367/B17/model/AbstractPowerUp.java
@@ -2,9 +2,19 @@ package chalmers.TDA367.B17.model;
 
 public abstract class AbstractPowerUp extends Entity {
 
+	private String name;
+	
 	public AbstractPowerUp(int id) {
 		super(id);
 		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
