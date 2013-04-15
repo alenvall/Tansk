2ffcc9da62f5390ccc7c30263260f1d7274195a3
killcone <vicsand@student.chalmers.se>
2013-04-15 16:05:47 +0200

    Added a class for AbstractProjectile.
    The class includes damage and duration for now.

---
+++ src/chalmers/TDA367/B17/model/AbstractProjectile.java
@@ -0,0 +1,32 @@
+package chalmers.TDA367.B17.model;
+
+import org.newdawn.slick.geom.Vector2f;
+
+public class AbstractProjectile extends MovableEntity {
+	
+	private double damage;
+	private double duration;
+
+	public AbstractProjectile(int id, Vector2f direction, double speed,
+			double maxSpeed, double minSpeed, double reverseSpeed) {
+		super(id, direction, speed, maxSpeed, minSpeed, reverseSpeed);
+		// TODO Auto-generated constructor stub
+	}
+
+	public double getDamage() {
+		return damage;
+	}
+
+	public double getDuration() {
+		return duration;
+	}
+
+	public void setDamage(double damage) {
+		this.damage = damage;
+	}
+
+	public void setDuration(double duration) {
+		this.duration = duration;
+	}
+
+}
