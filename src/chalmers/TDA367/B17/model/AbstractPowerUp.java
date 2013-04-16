14ccec67c6798dc8a7650a394c89038bf6fce911
killcone <vicsand@student.chalmers.se>
2013-04-16 13:38:40 +0200

    Added duration to AbstractPowerUp.

--- src/chalmers/TDA367/B17/model/AbstractPowerUp.java
+++ src/chalmers/TDA367/B17/model/AbstractPowerUp.java
@@ -3,6 +3,7 @@ package chalmers.TDA367.B17.model;
 public abstract class AbstractPowerUp extends Entity {
 
 	private String name;
+	private double duration;
 	
 	public AbstractPowerUp(int id) {
 		super(id);
@@ -15,6 +16,14 @@ public abstract class AbstractPowerUp extends Entity {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public double getDuration() {
+		return duration;
+	}
+
+	public void setDuration(double duration) {
+		this.duration = duration;
 	}
 
 }
