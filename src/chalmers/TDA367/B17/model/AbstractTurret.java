2f0fc6d6b86cadfad062f46a40f38e5ea8680de3
killcone <vicsand@student.chalmers.se>
2013-04-18 10:43:18 +0200

    The turret direction in AbstractTurret is now detemined by a float instead of a vector.

--- src/chalmers/TDA367/B17/model/AbstractTurret.java
+++ src/chalmers/TDA367/B17/model/AbstractTurret.java
@@ -1,22 +1,18 @@
 package chalmers.TDA367.B17.model;
 
-import org.newdawn.slick.geom.Vector2f;
-
 public abstract class AbstractTurret extends Entity {
 
-	private Vector2f turretDirection;
+	private float direction;
 	
 	public AbstractTurret(int id) {
 		super(id);
-		turretDirection = new Vector2f();
 	}
 
-	public Vector2f getTurretDirection() {
-		return turretDirection;
+	public float getDirection() {
+		return direction;
 	}
 
-	public void setTurretDirection(Vector2f turretDirection) {
-		this.turretDirection = turretDirection;
-		this.turretDirection = this.turretDirection.normalise();
+	public void setDirection(float direction) {
+		this.direction = direction % 360f;
 	}
 }
