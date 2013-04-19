9b2beaae715f73ee12e24efdabe77e9d55a9abe9
Kirtez <kirtez@gmail.com>
2013-04-19 16:26:56 +0200

    Removed turretDirection-vector, uses angle-float.

--- src/chalmers/TDA367/B17/model/AbstractTurret.java
+++ src/chalmers/TDA367/B17/model/AbstractTurret.java
@@ -3,26 +3,14 @@ package chalmers.TDA367.B17.model;
 import org.newdawn.slick.geom.Vector2f;
 
 public abstract class AbstractTurret extends Entity {
-
-	private Vector2f turretDirection;
 	private float angle;
 
 	public AbstractTurret(int id) {
 		super(id);
-		turretDirection = new Vector2f();
 	}
 
 	public Vector2f getImagePosition(){
 		return new Vector2f(position.x - 23, position.y - 22);
-	}
-
-	public Vector2f getTurretDirection() {
-		return turretDirection;
-	}
-
-	public void setTurretDirection(Vector2f turretDirection) {
-		this.turretDirection = turretDirection;
-		this.turretDirection = this.turretDirection.normalise();
 	}
 
 	public float getRotation() {
