ca436a60feeefec38759c624a2273f6faa5e51aa
killcone <vicsand@student.chalmers.se>
2013-04-23 16:08:35 +0200

    Cleaned up the constructors in DefaultProjectile and DefaultWeapon.

--- src/chalmers/TDA367/B17/model/DefaultProjectile.java
+++ src/chalmers/TDA367/B17/model/DefaultProjectile.java
@@ -4,8 +4,7 @@ import org.newdawn.slick.geom.Vector2f;
 
 public class DefaultProjectile extends AbstractProjectile {
 
-	public DefaultProjectile(Vector2f velocity, float maxSpeed,
-			float minSpeed, double damage, int duration) {
+	public DefaultProjectile() {
 		super(new Vector2f(1,1), 100, 0, 5, 1000);
 		speed = 20;
 	}
--- src/chalmers/TDA367/B17/model/DefaultWeapon.java
+++ src/chalmers/TDA367/B17/model/DefaultWeapon.java
@@ -1,16 +1,14 @@
 package chalmers.TDA367.B17.model;
 
-import org.newdawn.slick.geom.Vector2f;
-
 public class DefaultWeapon extends AbstractWeapon {
 
 	public DefaultWeapon() {
-		super(new DefaultProjectile(new Vector2f(5,5), 10, 5, 5, 10), 1000);
+		super(new DefaultProjectile(), 300);
 	}
 
 	@Override
 	public AbstractProjectile createProjectile() {
-		return new DefaultProjectile(new Vector2f(5,5), 10, 5, 5, 10);
+		return new DefaultProjectile();
 	}
 
 }
