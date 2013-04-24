f53f3aaef2279085e39bbc0ad18dd9ba4b86bc23
killcone <vicsand@student.chalmers.se>
2013-04-24 14:39:26 +0200

    Edited some attributes in ShotgunProjectile.

--- src/chalmers/TDA367/B17/model/ShotgunProjectile.java
+++ src/chalmers/TDA367/B17/model/ShotgunProjectile.java
@@ -5,13 +5,13 @@ import org.newdawn.slick.geom.Vector2f;
 public class ShotgunProjectile extends AbstractProjectile {
 
 	public ShotgunProjectile() {
-		super(new Vector2f(1,1), 100, 0, 5, 500);
-		speed = 35;
+		super(new Vector2f(1,1), 100, 0, 5, 350);
+		speed = 30;
 	}
-
+	
 	public void update(int delta){
 		// add spread
-		int randomAngle = (int) (Math.random() * 80 - 40);
+		int randomAngle = (int) (Math.random() * 20 - 10);
 		direction.add(randomAngle);
 		super.update(delta);
 		direction.add(-randomAngle);
