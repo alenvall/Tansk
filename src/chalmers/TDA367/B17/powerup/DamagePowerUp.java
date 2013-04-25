bb5c9a413b21b9d47d3d14a15e3745033f72f2b8
killcone <vicsand@student.chalmers.se>
2013-04-26 00:01:16 +0200

    A powerUp for increased damage, work in progress!

---
+++ src/chalmers/TDA367/B17/powerup/DamagePowerUp.java
@@ -0,0 +1,22 @@
+package chalmers.TDA367.B17.powerup;
+
+import chalmers.TDA367.B17.model.AbstractPowerUp;
+
+
+public class DamagePowerUp extends AbstractPowerUp{
+
+	public static int MULTIPLIER = 2;
+	
+	public DamagePowerUp() {
+		super();
+		effectDuration = 7000;
+	}
+
+	public void effect(){
+		
+	}
+	
+	public void endEffect(){
+		
+	}
+}
