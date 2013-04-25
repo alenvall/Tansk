ac1a6627887b685f0c6b4b32a12ab2cdce4bf2e1
killcone <vicsand@student.chalmers.se>
2013-04-25 12:30:14 +0200

    Added FlamethrowerProjectile class.

---
+++ src/chalmers/TDA367/B17/model/FlamethrowerProjectile.java
@@ -0,0 +1,20 @@
+package chalmers.TDA367.B17.model;
+
+import org.newdawn.slick.geom.Vector2f;
+
+public class FlamethrowerProjectile extends AbstractProjectile {
+
+	public FlamethrowerProjectile() {
+		super(new Vector2f(1,1), 100, 0, 5, 500);
+		setSpeed(15);
+	}
+
+	
+	public void update(int delta){
+		// add spread
+		int randomAngle = (int) (Math.random() * 80 - 40);
+		direction.add(randomAngle);
+		super.update(delta);
+		direction.add(-randomAngle);
+	}	
+}
