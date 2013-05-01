bd3298a115f33d05e354fac87ab21f2367f663bc
killcone <vicsand@student.chalmers.se>
2013-05-01 19:29:26 +0200

    The shape is now set in AbstractObstacle to prevent duplicate code

--- src/chalmers/TDA367/B17/terrain/BrownWall.java
+++ src/chalmers/TDA367/B17/terrain/BrownWall.java
@@ -11,9 +11,7 @@ public class BrownWall extends AbstractObstacle {
 	Sound debugWallHit;
 	
 	public BrownWall(Vector2f size, Vector2f position) {
-		super(size);
-		setPosition(position);
-		setShape(new Rectangle(getPosition().getX()-getSize().getX()/2, getPosition().getY()-getSize().getY()/2, getSize().getX(), getSize().getY()));
+		super(size, position);
 		try{
 			debugWallHit = new Sound("data/wall.wav");
 		}catch(Exception e){
@@ -25,9 +23,6 @@ public class BrownWall extends AbstractObstacle {
 	public void didCollideWith(Entity entity){
 		if(entity instanceof AbstractProjectile){
 			entity.destroy();
-			debugWallHit.play();
-		}else if(entity instanceof AbstractTank){
-			((AbstractTank)entity).setSpeed(-((AbstractTank)entity).getSpeed());
 			debugWallHit.play();
 		}
 	}
