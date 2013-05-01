3ba51a92c0a4d57e194f6c030c6de48ea3f83e62
killcone <vicsand@student.chalmers.se>
2013-05-01 19:27:14 +0200

    AbstractObstacle now has a shape when created

--- src/chalmers/TDA367/B17/model/AbstractObstacle.java
+++ src/chalmers/TDA367/B17/model/AbstractObstacle.java
@@ -1,11 +1,13 @@
 package chalmers.TDA367.B17.model;
 
+import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Vector2f;
 
 public class AbstractObstacle extends Entity {
 
-	public AbstractObstacle(Vector2f size) {
+	public AbstractObstacle(Vector2f size, Vector2f position) {
 		setSize(size);
+		setShape(new Rectangle(position.getX()-size.getX()/2, position.getY()-size.getY()/2, size.getX(), size.getY()));
 	}
 
 }
