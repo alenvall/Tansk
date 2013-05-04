aeb16bbf19e95bda5568c32a1802be9d90bcbd79
killcone <vicsand@student.chalmers.se>
2013-05-04 13:47:07 +0200

    The shield now keeps track of the tank it is shielding and has health
    The purpose of this is that the projectiles fired from the tank, that
    has the shield activated, shouldn't damage the shield.

--- src/chalmers/TDA367/B17/powerups/Shield.java
+++ src/chalmers/TDA367/B17/powerups/Shield.java
@@ -4,23 +4,44 @@ import org.newdawn.slick.geom.Circle;
 import org.newdawn.slick.geom.Vector2f;
 
 import chalmers.TDA367.B17.model.AbstractProjectile;
+import chalmers.TDA367.B17.model.AbstractTank;
 import chalmers.TDA367.B17.model.Entity;
 
 public class Shield extends Entity {
+	
+	private double health;
+	private AbstractTank absTank;
 
-	public Shield(Vector2f position) {
+	public Shield(Vector2f position, AbstractTank absTank) {
 		setSize(new Vector2f(15, 5));
 		setPosition(position);
 		setShape(new Circle(position.x, position.y, 100));
 		setSize(new Vector2f(100,100));
 		spriteID = "shield";
 		active = true;
-	}
-	
-	public void didCollideWith(Entity entity){
-		if(entity instanceof AbstractProjectile){
-			entity.destroy();
-		}
+		
+		this.health = 50;
+		this.absTank = absTank;
 	}
 
+	public double getHealth() {
+		return health;
+	}
+
+	public void setHealth(double health) {
+		this.health = health;
+	}
+
+	public void didCollideWith(Entity entity){
+		if(entity instanceof AbstractProjectile){
+			if(!(((AbstractProjectile)entity).getTank() == absTank)){
+				setHealth(getHealth() - ((AbstractProjectile)entity).getDamage());
+				((AbstractProjectile)entity).destroy();
+				
+				if(getHealth() <= 0){
+					active = false;
+				}
+			}
+		}
+	}
 }
