b10b8a3f10f20098e2961b90fba3fc2a5cb9b570
killcone <vicsand@student.chalmers.se>
2013-05-04 11:48:59 +0200

    Moved the current, and only tank, to a new package

--- src/chalmers/TDA367/B17/tanks/DefaultTank.java
+++ src/chalmers/TDA367/B17/tanks/DefaultTank.java
@@ -1,13 +1,16 @@
-package chalmers.TDA367.B17.model;
+package chalmers.TDA367.B17.tanks;
 
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Vector2f;
+
+import chalmers.TDA367.B17.model.AbstractTank;
+import chalmers.TDA367.B17.model.Player;
 import chalmers.TDA367.B17.weapons.DefaultTurret;
 
 public class DefaultTank extends AbstractTank {
 
-	public DefaultTank(Vector2f direction, float maxSpeed, float minSpeed) {
-		super(direction, maxSpeed, minSpeed);
+	public DefaultTank(Vector2f direction, float maxSpeed, float minSpeed, Player player) {
+		super(direction, maxSpeed, minSpeed, player);
 		setShape(new Rectangle(100, 150, 65, 85));
 		turretOffset = 6;
 		turret = new DefaultTurret(this);
