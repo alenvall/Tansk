6475a731f2cc73aa03e3230b1fed3faf8595d724
killcone <vicsand@student.chalmers.se>
2013-05-05 19:53:53 +0200

    Edited to fit the latest change to TankSpawner
    TankSpawner is no longer a singleton. It's from now on an object in the
    current World.

--- src/chalmers/TDA367/B17/spawnpoints/TankSpawnPoint.java
+++ src/chalmers/TDA367/B17/spawnpoints/TankSpawnPoint.java
@@ -3,6 +3,7 @@ package chalmers.TDA367.B17.spawnpoints;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Vector2f;
 
+import chalmers.TDA367.B17.controller.GameController;
 import chalmers.TDA367.B17.model.AbstractSpawnPoint;
 import chalmers.TDA367.B17.model.AbstractTank;
 import chalmers.TDA367.B17.model.Entity;
@@ -13,7 +14,7 @@ public class TankSpawnPoint extends AbstractSpawnPoint {
 	
 	public TankSpawnPoint(Vector2f position) {
 		super(position);
-		TankSpawner.getInstance().addTankSpawnPoint(this);
+		GameController.getInstance().getWorld().getTankSpawner().addTankSpawnPoint(this);
 		spriteID = "tank_spawnpoint";
 		Vector2f size = new Vector2f(65f, 85f);
 		setShape(new Rectangle(position.getX()-size.getX()/2, position.getY()-size.getY()/2, size.getX(), size.getY()));
