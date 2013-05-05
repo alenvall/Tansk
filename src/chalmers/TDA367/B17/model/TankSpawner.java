9cea97fa6617fd8094fb2c5c253c0830e22cf1e7
killcone <vicsand@student.chalmers.se>
2013-05-05 19:42:59 +0200

    Moved TankSpawner to the model package
    TankSpawner is no longer a singleton class but is now an object stored
    in World.

--- src/chalmers/TDA367/B17/model/TankSpawner.java
+++ src/chalmers/TDA367/B17/model/TankSpawner.java
@@ -1,28 +1,18 @@
-package chalmers.TDA367.B17.spawnpoints;
+package chalmers.TDA367.B17.model;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import chalmers.TDA367.B17.model.Player;
+import chalmers.TDA367.B17.spawnpoints.TankSpawnPoint;
 
 public class TankSpawner{
 	
-	private static TankSpawner instance;
 	private List<TankSpawnPoint> tankSpawnPoints;
 	private List<Player> playerList;
 	
-	private TankSpawner(){
-		super();
+	public TankSpawner(){
 		tankSpawnPoints = new ArrayList<TankSpawnPoint>();
 		playerList = new ArrayList<Player>();
-	}
-
-	public static TankSpawner getInstance(){
-		if(instance==null){
-			instance = new TankSpawner();
-		}
-		
-		return instance;
 	}
 	
 	public void update(int delta){
