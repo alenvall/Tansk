0507543665368db90e3a696ee6a58aab51aba894
killcone <vicsand@student.chalmers.se>
2013-05-06 16:57:02 +0200

    Checking for collision on tankspawnpoints in TankSpawner

--- src/chalmers/TDA367/B17/model/TankSpawner.java
+++ src/chalmers/TDA367/B17/model/TankSpawner.java
@@ -1,8 +1,12 @@
 package chalmers.TDA367.B17.model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
+import chalmers.TDA367.B17.controller.GameController;
 import chalmers.TDA367.B17.spawnpoints.TankSpawnPoint;
 
 public class TankSpawner{
@@ -20,6 +24,15 @@ public class TankSpawner{
 			Player p = playerList.get(i);
 			p.setRespawnTimer(p.getRespawnTimer() - delta);
 			if(p.getRespawnTimer() <= 0){
+				GameController controller = GameController.getInstance();
+				//Bad place to run this
+				Iterator<Entry<Integer, Entity>> iterator = controller.getWorld().getEntities().entrySet().iterator();
+				while(iterator.hasNext()){
+					Map.Entry<Integer, Entity> entry = (Entry<Integer, Entity>) iterator.next();
+					Entity entity = entry.getValue();
+					if(entity instanceof TankSpawnPoint)
+						controller.getWorld().checkCollisionsFor(entity);
+				}
 				getTankSpawnPoint(p);
 				playerList.remove(i);
 			}
