ff4998e9c036e49678daf8ab232f96ebfb131709
killcone <vicsand@student.chalmers.se>
2013-05-11 15:25:04 +0200

    Reworked the shockwaveprojectile (sort of)
    The initial projectile now detonates on walls and other tank instead of
    just be destroyed. This should make the weapon a little bit more useful
    (and fun). Also, the initial projectile no longer deals any damage. The
    damage of the shockwave itself might have to be tweaked since it's a
    little overpowered at the moment. A solution might have to be
    implemented where there's a maximum damage that the shockwave can do to
    a single tank.

--- src/chalmers/TDA367/B17/weapons/ShockwaveProjectile.java
+++ src/chalmers/TDA367/B17/weapons/ShockwaveProjectile.java
@@ -2,32 +2,52 @@ package chalmers.TDA367.B17.weapons;
 
 import org.newdawn.slick.geom.Vector2f;
 
+import chalmers.TDA367.B17.model.AbstractObstacle;
 import chalmers.TDA367.B17.model.AbstractProjectile;
 import chalmers.TDA367.B17.model.AbstractTank;
+import chalmers.TDA367.B17.model.Entity;
+import chalmers.TDA367.B17.model.MapBounds;
 
 public class ShockwaveProjectile extends AbstractProjectile {
 
 	private boolean activated = false;
 	
 	public ShockwaveProjectile(AbstractTank tank, Vector2f position) {
-		super(tank, position, new Vector2f(1,1), 100, 0, 0.5, 1500);
+		super(tank, position, new Vector2f(1,1), 100, 0, 0, 1500);
 		setSpeed(0.2f);
 		setSize(new Vector2f(8,10));
 		setPosition(position);
 		spriteID = "shockwave_proj";
 	}
 	
+	public void detonate(){
+		activated = true;
+		for(int i = 0; i < 40; i++){
+			AbstractProjectile projectile = new ShockwaveSecondaryProjectile(getTank(), new Vector2f(getPosition()));
+			projectile.setDirection(new Vector2f(getRotation() + i*9));
+			
+			getTank().addProjectile(projectile);
+		}
+		this.destroy();
+	}
+	
 	@Override
 	public void update(int delta){
 		if(getDurationTimer() <= 20 && !activated){
-			activated = true;
-			for(int i = 0; i < 40; i++){
-				AbstractProjectile projectile = new ShockwaveSecondaryProjectile(getTank(), new Vector2f(getPosition()));
-				projectile.setDirection(new Vector2f(getRotation() + i*9));
-				
-				getTank().addProjectile(projectile);
-			}
+			detonate();
 		}
 		super.update(delta);
+	}
+	
+	public void didCollideWith(Entity entity){
+		if(entity instanceof MapBounds || entity instanceof AbstractObstacle || entity instanceof AbstractTank){
+			if(entity instanceof AbstractTank){
+				if(!activated && (AbstractTank)entity != getTank()){
+					detonate();
+				}
+			} else if(!activated){
+				detonate();
+			}
+		}
 	}
 }
