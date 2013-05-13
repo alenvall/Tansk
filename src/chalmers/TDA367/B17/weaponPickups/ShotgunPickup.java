7b12119358be13c2da03de84a133d1e1ef5d90f4
Tobias Olausson <tobbe_o_1993@hotmail.com>
2013-05-13 15:14:39 +0200

    Revert "Revert "Added very temporary FlamethrowerIcon""
    This reverts commit 3bd2541e56d3dc378e1d8f953e98b62b2e2bd96a.

---
+++ src/chalmers/TDA367/B17/weaponPickups/ShotgunPickup.java
@@ -0,0 +1,20 @@
+package chalmers.TDA367.B17.weaponPickups;
+
+import org.newdawn.slick.geom.Vector2f;
+
+import chalmers.TDA367.B17.model.AbstractTank;
+import chalmers.TDA367.B17.weapons.ShotgunTurret;
+
+public class ShotgunPickup extends AbstractWeaponPickup{
+
+	public ShotgunPickup(Vector2f position, AbstractTank tank) {
+		super(position);
+		spriteID = "ShotgunIcon";
+	}
+	
+	@Override
+	public void activate(AbstractTank absTank){
+		super.activate(absTank);
+		absTank.setTurret(new ShotgunTurret(absTank));
+	}
+}
