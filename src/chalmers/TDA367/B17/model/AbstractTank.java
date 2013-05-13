52f87a88818ddef3bdf363b841688d0a76bdf6f4
killcone <vicsand@student.chalmers.se>
2013-05-13 19:12:37 +0200

    Added some javadoc and removed some old code
    More javadoc for this class has to be added later on.

--- src/chalmers/TDA367/B17/model/AbstractTank.java
+++ src/chalmers/TDA367/B17/model/AbstractTank.java
@@ -22,6 +22,13 @@ public abstract class AbstractTank extends MovableEntity {
 	private double lastDir;
 	private Player player;
 	
+	/**
+	 * Create a new AbstractTank.
+	 * @param velocity The velocity of this tank
+	 * @param maxSpeed The maximum movement speed of this tank
+	 * @param minSpeed The minimum movement speed of this tank
+	 * @param player The owning player of this tank
+	 */
 	public AbstractTank(Vector2f velocity, float maxSpeed, float minSpeed, Player player) {
 		super(velocity, maxSpeed, minSpeed);
 		turnSpeed = 0.15f;
@@ -34,30 +41,58 @@ public abstract class AbstractTank extends MovableEntity {
 		this.player = player;
 	}
 	
+	/**
+	 * Get the rotation of this tank.
+	 * @return The rotation of this tank
+	 */
 	public double getRotation(){
 		return direction.getTheta() + 90;
 	}
 	
+	/**
+	 * Get the name of this tank.
+	 * @return The name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Set the name of this tank.
+	 * @param name The new name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Get the current health of this tank.
+	 * @return The health of this tank
+	 */
 	public double getHealth() {
 		return health;
 	}
 
+	/**
+	 * Set the health of this tank.
+	 * @param health The new health
+	 */
 	public void setHealth(double health) {
 		this.health = health;
 	}
 
+	/**
+	 * Get the power up that is currently active for this tank.
+	 * @return The current power up
+	 */
 	public AbstractPowerUp getCurrentPowerUp() {
 		return currentPowerUp;
 	}
 
+	/**
+	 * Set the current power up of this tank.
+	 * @param currentPowerUp The new power up
+	 */
 	public void setCurrentPowerUp(AbstractPowerUp currentPowerUp) {
 		if(currentPowerUp != null){
 			if(this.currentPowerUp != null){
@@ -69,34 +104,58 @@ public abstract class AbstractTank extends MovableEntity {
 		}
 	}
 
+	/**
+	 * Get the turn speed of this tank.
+	 * @return The turn speed
+	 */
 	public float getTurnSpeed() {
 		return turnSpeed;
 	}
 
+	/**
+	 * Set the turn speed of this tank.
+	 * @param turnSpeed The new turn speed
+	 */
 	public void setTurnSpeed(float turnSpeed) {
 		this.turnSpeed = turnSpeed;
 	}
 	
+	/**
+	 * Get the turret of this tank.
+	 * @return The turret
+	 */
 	public AbstractTurret getTurret() {
 		return turret;
 	}
 
+	/**
+	 * Set the turret of this tank.
+	 * @param turret The new turret
+	 */
 	public void setTurret(AbstractTurret turret) {
 		if(getTurret() != null)
 			getTurret().destroy();
 		this.turret = turret;
 	}
 
+	/**
+	 * Turn the tank left, adjusting the direction it's facing.
+	 * @param delta The time that has passed since the last update in milliseconds
+	 */
 	public void turnLeft(int delta){
 		lastDir = getDirection().getTheta();
 		setDirection(getDirection().add(-turnSpeed * delta * (Math.abs(getSpeed())*0.2f + 0.7)));
 	}
 	
+	/**
+	 * Turn the tank right, adjusting the direction it's facing.
+	 * @param delta The time that has passed since the last update in milliseconds
+	 */
 	public void turnRight(int delta){
 		lastDir = getDirection().getTheta();
 		setDirection(getDirection().add(turnSpeed * delta * (Math.abs(getSpeed())*0.2f + 0.7)));
 	}
-
+	
 	public float getTurretOffset() {
 	    return turretOffset;
     }
@@ -125,12 +184,6 @@ public abstract class AbstractTank extends MovableEntity {
 	@Override
 	public void didCollideWith(Entity entity){
 		if(entity instanceof MapBounds || entity instanceof AbstractTank || entity instanceof AbstractObstacle){
-			if(entity instanceof AbstractTank){
-				/*
-				if(Math.abs(((AbstractTank)entity).getSpeed()) > Math.abs(getSpeed())){
-					setSpeed(-getSpeed());
-				}*/
-			}
 			if(!lastPos.equals(getPosition())){
 				setPosition(lastPos);
 				setSpeed(-getSpeed());
