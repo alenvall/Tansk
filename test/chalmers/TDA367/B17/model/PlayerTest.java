89f8a52bc5aaf1b265dea3661d9151c0ba345e93
Victor <vicsand@student.chalmers.se>
2013-05-19 18:54:36 +0200

    Updated JUnit test for Entity, MovableEntity and Player

--- test/chalmers/TDA367/B17/model/PlayerTest.java
+++ test/chalmers/TDA367/B17/model/PlayerTest.java
@@ -32,7 +32,49 @@ public class PlayerTest {
 		player.setScore(123);
 		assertTrue(player.getScore()==123);
 	}
+	
+	@Test
+	public void testSetEliminated(){
+		Player player = new Player("Test Player");
+		player.setEliminated(false);
+		assertTrue(!player.isEliminated());
+	}
 
+	@Test
+	public void testSetRespawnTime(){
+		Player player = new Player("Test Player");
+		player.setRespawnTime(123);
+		assertTrue(player.getRespawnTime() == 123);
+	}
+	
+	@Test
+	public void testSetLives(){
+		Player player = new Player("Test Player");
+		player.setLives(12);
+		assertTrue(player.getLives() == 12);
+	}
+	
+	@Test
+	public void testSetRespawnTimer(){
+		Player player = new Player("Test Player");
+		player.setRespawnTimer(123);
+		assertTrue(player.getRespawnTimer() == 123);
+	}
+	
+	@Test
+	public void testSetActive(){
+		Player player = new Player("Test Player");
+		player.setActive(false);
+		assertTrue(!player.isActive());
+	}
+	
+	@Test
+	public void testSetInputStatus(){
+		Player player = new Player("Test Player");
+		player.setInputStatus(Player.INPT_W, true);
+		assertTrue(player.getInput().get(0).booleanValue() == true);
+	}
+	
 	@BeforeClass
 	public static void beforeClass(){
 		System.out.println("Before class!");
