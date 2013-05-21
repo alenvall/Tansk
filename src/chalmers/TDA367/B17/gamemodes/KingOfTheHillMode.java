b94ac90de449356bce4f3352f116f2058ba19e1e
Tobias Olausson <tobbe_o_1993@hotmail.com>
2013-05-21 17:55:53 +0200

    Worked on KingOfTheHillMode

--- src/chalmers/TDA367/B17/gamemodes/KingOfTheHillMode.java
+++ src/chalmers/TDA367/B17/gamemodes/KingOfTheHillMode.java
@@ -1,5 +1,26 @@
 package chalmers.TDA367.B17.gamemodes;
 
-public class KingOfTheHillMode extends ScoreBasedGame{
+import org.newdawn.slick.geom.Shape;
 
+import chalmers.TDA367.B17.model.Player;
+
+public class KingOfTheHillMode extends ScoreBasedGame{
+	
+	private Shape pointZone;
+	
+	public KingOfTheHillMode(Shape shape){
+		pointZone = shape;
+	}
+	
+	
+	@Override
+	public void update(int delta){
+		super.update(delta);
+		for(Player p: players){
+			if(pointZone.contains(p.getTank().getShape())){
+				incrementPlayerScore(p);
+			}
+		}
+		
+	}
 }
