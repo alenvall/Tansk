b932ccd7d1387d8595c16360518fafed049208fe
Tobias <tobbe_o_1993@hotmail.com>
2013-05-22 15:29:46 +0200

    fully implemented KOTH

--- src/chalmers/TDA367/B17/gamemodes/KingOfTheHillMode.java
+++ src/chalmers/TDA367/B17/gamemodes/KingOfTheHillMode.java
@@ -1,26 +1,56 @@
 package chalmers.TDA367.B17.gamemodes;
 
-import org.newdawn.slick.geom.Shape;
+import java.util.HashMap;
 
+import org.newdawn.slick.geom.Vector2f;
+
+import chalmers.TDA367.B17.controller.GameController;
+import chalmers.TDA367.B17.model.KingOfTheHillZone;
 import chalmers.TDA367.B17.model.Player;
 
 public class KingOfTheHillMode extends ScoreBasedGame{
 	
-	private Shape pointZone;
+	private KingOfTheHillZone pointZone;
+	private HashMap<Player, Integer> playersInZone;
 	
-	public KingOfTheHillMode(Shape shape){
-		pointZone = shape;
+	public KingOfTheHillMode(Vector2f position){
+		playersInZone = new HashMap<Player, Integer>();
+		pointZone = new KingOfTheHillZone(GameController.getInstance().generateID() , position);
+		setScoreLimit(15);
 	}
-	
 	
 	@Override
 	public void update(int delta){
 		super.update(delta);
 		for(Player p: players){
-			if(pointZone.contains(p.getTank().getShape())){
-				incrementPlayerScore(p);
+			if(!p.isActive()){
+				p.spawnTank();
+				
+			}
+			if(p.getTank() != null && playerInZone(p)){
+				if(!playersInZone.containsKey(p)){
+					playersInZone.put(p, 0);
+				}else{
+					Integer timeInZone = playersInZone.get(p);
+					timeInZone +=delta;
+					GameController.getInstance().getConsole().addMsg("" + timeInZone);
+					if(timeInZone > 1000){
+						incrementPlayerScore(p);
+						timeInZone = 0;
+					}
+					playersInZone.put(p, timeInZone);
+				}
 			}
 		}
-		
+		for(Player p: players){
+			if(p.getScore() >= getScoreLimit()){
+				setGameOver(true);
+				addWinningPlayer(p);
+			}
+		}
+	}
+	
+	public boolean playerInZone(Player p){
+		return pointZone.getShape().contains(p.getTank().getShape()) || pointZone.getShape().intersects(p.getTank().getShape());
 	}
 }
