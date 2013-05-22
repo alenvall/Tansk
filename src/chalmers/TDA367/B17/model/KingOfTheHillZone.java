7f10a96a02ea2783570ec20fd1f905245250dec2
Tobias <tobbe_o_1993@hotmail.com>
2013-05-22 14:09:52 +0200

    KingOfTheHIllZone adds itself to world

--- src/chalmers/TDA367/B17/model/KingOfTheHillZone.java
+++ src/chalmers/TDA367/B17/model/KingOfTheHillZone.java
@@ -2,13 +2,15 @@ package chalmers.TDA367.B17.model;
 
 import org.newdawn.slick.geom.*;
 
+import chalmers.TDA367.B17.controller.GameController;
+
 public class KingOfTheHillZone extends Entity {
-	
-	private Shape shape;
 	
 	public KingOfTheHillZone(int id, Vector2f position){
 		super(id);
 		shape = new Circle(position.x, position.y, 20);
 		spriteID = "king_of_the_hill_zone";
+		GameController.getInstance().getWorld().addEntity(this);
+		renderLayer = RenderLayer.FIRST;
 	}
 }
