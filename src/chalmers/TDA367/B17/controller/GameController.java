d554f2c015562dc8988364e46fddc5c59aa11495
Kirtez <kirtez@gmail.com>
2013-05-25 13:07:02 +0200

    Code cleanup

--- src/chalmers/TDA367/B17/controller/GameController.java
+++ src/chalmers/TDA367/B17/controller/GameController.java
@@ -2,8 +2,6 @@ package chalmers.TDA367.B17.controller;
 
 import java.awt.Dimension;
 
-import org.newdawn.slick.geom.Vector2f;
-
 import chalmers.TDA367.B17.Tansk;
 import chalmers.TDA367.B17.console.Console;
 import chalmers.TDA367.B17.event.GameEvent;
@@ -62,7 +60,7 @@ public class GameController {
 
 		GameController.getInstance().getConsole().addMsg("GameController.newGame()");
 		
-		gameMode = new KingOfTheHillMode(new Vector2f(512,384));
+		gameMode = new KingOfTheHillMode();
 		gameMode.init(rounds, playerLives, spawnTime, roundTime, gameTime);
 
 	}
