c1f56272efbceda4f822e90dbfce5a3a538149fc
Tobias Olausson <tobbe_o_1993@hotmail.com>
2013-05-26 13:27:16 +0200

    Added buttons to scoreboard

--- src/chalmers/TDA367/B17/view/Scoreboard.java
+++ src/chalmers/TDA367/B17/view/Scoreboard.java
@@ -13,9 +13,25 @@ import chalmers.TDA367.B17.model.Player;
 public class Scoreboard {
 
 	private Font scoreboardFont;
+	private MenuButton toMenuButton;
+	private MenuButton restartButton;
+	private Vector2f position;
+	private int width;
+	private int height;
 	
 	public Scoreboard(){
-
+		width = 350;
+		height = 350;
+		position = new Vector2f(Tansk.SCREEN_WIDTH/2-width/2, Tansk.SCREEN_HEIGHT/2-height/2);
+		
+		toMenuButton = new MenuButton((int)(position.x + 15), (int)(position.y + height-60), GameController.getInstance().getImageHandler().getSprite("button_menu"),
+				GameController.getInstance().getImageHandler().getSprite("button_menu_pressed"),
+				GameController.getInstance().getImageHandler().getSprite("button_menu_hover"));
+		
+		restartButton = new MenuButton((int)(position.x + width - 15), (int)(position.y + height-60), GameController.getInstance().getImageHandler().getSprite("button_restart"),
+				GameController.getInstance().getImageHandler().getSprite("button_restart_pressed"),
+				GameController.getInstance().getImageHandler().getSprite("button_restart_hover"));
+		
 		scoreboardFont = new TrueTypeFont(new java.awt.Font("Verdana", java.awt.Font.PLAIN, 22), true);
 		
 	}
@@ -23,18 +39,19 @@ public class Scoreboard {
 	public void render(Graphics g){
 		g.setLineWidth(15);
 		g.setColor(new Color(100, 100, 100, 255));
-		int tmpSideLength = 350;
 		int tmpYOffset = 10;
-		Vector2f tmpPosition = new Vector2f(Tansk.SCREEN_WIDTH/2-tmpSideLength/2, Tansk.SCREEN_HEIGHT/2-tmpSideLength/2);
-		g.fillRect(tmpPosition.x, tmpPosition.y, tmpSideLength, tmpSideLength);
+		g.fillRect(position.x, position.y, width, height);
 		g.setColor(Color.black);
-		g.drawRect(tmpPosition.x, tmpPosition.y, tmpSideLength, tmpSideLength);
+		g.drawRect(position.x, position.y, width, height);
 		g.setLineWidth(1);
 		
 		g.setFont(scoreboardFont);
 		for(Player p: GameController.getInstance().getGameMode().getPlayerList()){
-			g.drawString(p.getName() + ": " + p.getScore(), tmpPosition.x+70, tmpPosition.y+tmpYOffset);
+			g.drawString(p.getName() + ": " + p.getScore(), position.x+70, position.y+tmpYOffset);
 			tmpYOffset += 30;
 		}
+		
+		toMenuButton.draw();
+		restartButton.draw();
 	}
 }
