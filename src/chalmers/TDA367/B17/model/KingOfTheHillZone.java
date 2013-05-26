31a36a8a786e42ade0ed41f8d5fc64689c24ccc1
Tobias Olausson <tobbe_o_1993@hotmail.com>
2013-05-26 17:20:48 +0200

    Added getPosition to KOTH zone

--- src/chalmers/TDA367/B17/model/KingOfTheHillZone.java
+++ src/chalmers/TDA367/B17/model/KingOfTheHillZone.java
@@ -1,5 +1,7 @@
 package chalmers.TDA367.B17.model;
 
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.geom.*;
 
 import chalmers.TDA367.B17.controller.GameController;
@@ -14,11 +16,19 @@ public class KingOfTheHillZone extends Entity {
 	 * @param id The id
 	 * @param position The position
 	 */
+	
+	private Vector2f position;
+	
 	public KingOfTheHillZone(int id, Vector2f position){
 		super(id);
+		this.position = position;
 		shape = new Circle(position.x, position.y, 45);
 		spriteID = "king_of_the_hill_zone";
 		GameController.getInstance().getWorld().addEntity(this);
 		renderLayer = RenderLayer.FIRST;
+	}
+	
+	public Vector2f getPosition(){
+		return position;
 	}
 }
